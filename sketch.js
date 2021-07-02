123785672c0bced485bb55538583dc7c71b032c7
GreyRobin <86514645+GreyRobin@users.noreply.github.com>
2021-07-02 16:37:09 +1000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,28 +1,40 @@
-
-  function preload(){
-    //pre-load images
-    var runner, runnerani
-    var path, pathani
-    runnerani = loadAnimation("Runner-1.png" , "Runner-2.png")
-    pathani = loadAnimation("path.png")
-    var leftwall
-    var rightwall
-  }
-
-  function setup(){
-    createCanvas(400,400);
-    //create sprites here
-    runner = createSprite(200,300,50,50)
-    runner = addAnimation(runnerani)
-    path = createSprite(200,200,200,200)
-    path = addAnimation(pathani)
-  }
-
-  function draw() {
-    background(0);
-    ground.velocityY = -2
-    if(path.y<0){
-      path.y=path.height/2
-      runnerani.x = World.mouseX
-
+var path,boy, leftBoundary,rightBoundary; 
+var pathImg,boyImg; 
+var i; 
+function preload(){
+  pathImg = loadImage("path.png"); 
+  boyImg = loadAnimation("Runner-1.png","Runner-2.png"); 
+}
+function setup(){ createCanvas(400,400); 
+  // Moving background
+  path=createSprite(200,200); 
+  path.addImage(pathImg); 
+  path.velocityY = 4; 
+  path.scale=1.2; 
+  //creating boy running 
+  boy = createSprite(180,340,30,30); 
+  boy.scale=0.08; 
+  boy.addAnimation("JakeRunning",boyImg); 
+  // create left Boundary 
+  leftBoundary=createSprite(0,0,100,800); 
+  leftBoundary.visible = false; 
+  //create right Boundary 
+  rightBoundary=createSprite(410,0,100,800); 
+  rightBoundary.visible = false; 
+}
+  
+  function draw() { 
+    background(0); 
+    path.velocityY = 4; 
+    // boy moving on Xaxis with mouse 
+    boy.x = World.mouseX; 
+    edges= createEdgeSprites(); 
+    boy.collide(edges[3]); 
+    boy.collide(leftBoundary); 
+    boy.collide(rightBoundary); 
+    //code to reset the background 
+    if(path.y > 400 )
+    { path.y = height/2; 
+    } 
+    drawSprites(); 
   }
